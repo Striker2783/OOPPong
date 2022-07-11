2b23ad042e0e48b1c45ad99964d493fbe21a40b3
Striker2783 <owenxingli@gmail.com>
2022-07-11 10:03:57 -0400

    Score Text Aligned

--- Classes/Score.py
+++ Classes/Score.py
@@ -9,8 +9,9 @@ class Score(pygame.sprite.Sprite):
         self.score = score
         font_object = pygame.font.Font(font, font_size)
         self.image = font_object.render(str(self.score), True, color)
-        test = pygame.Surface((0, 0))
-        rect = pygame.draw.rect(test, color, [0, 0, 0, 0])
-        self.rect = rect
+        size = self.image.get_size()
+        test = pygame.Surface((size[0], size[1]))
+        pygame.draw.rect(test, color, [0, 0, size[0], size[1]])
+        self.rect = test.get_rect()
         self.rect.__setattr__(position[0], position[1])
-        print(rect)
+        print(self.rect.topright)
